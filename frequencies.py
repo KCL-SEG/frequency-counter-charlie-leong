de1de68002494f03280574746f73e7d6aaac4246
Charlotte Hoi Yi Leong <charlotte.leong@kcl.ac.uk>
2022-10-01 12:48:11 +0100

    change mistyped variable names

--- frequencies.py
+++ frequencies.py
@@ -6,7 +6,7 @@ def frequencies(items):
     for item in items:
         if item in frequencies:
             current_value = frequencies.get(str(item))
-            frequency.update({str(item):current_value + 1})
+            frequencies.update({str(item):current_value + 1})
         else:
-            frequency.update({str(item):1})
+            frequencies.update({str(item):1})
     return frequencies
